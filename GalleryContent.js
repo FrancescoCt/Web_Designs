51dd02f6ce26782db040613131ff6035f76a0726
Francesco Catania <81304650+FrancescoCt@users.noreply.github.com>
2021-12-30 21:30:12 +0100

    Add files via upload

---
+++ GalleryContent.js
@@ -0,0 +1,27 @@
+let contents = [
+	{
+		title: "Howl's Moving Castle",
+		image: "https://wallpapercave.com/wp/wp4091573.jpg",
+		genre: ' Fantasy',
+		liked: 'No',
+		rating: '4',
+		author: 'Hayao Miyazaki',
+		editorHouse: "Studio-ghibli",
+		description: "Bellissimo anime meravigliosi contenuti, paesaggi mozzafiato",
+		urlLink: "https://www.youtube.com/watch?v=Guxx_KTm_6M",
+		year: '2004'
+	},
+	{
+		title: "Dr. Stone",
+		image: "https://wallpapercave.com/wp/wp5174836.jpg",
+		genre: ' Shonen',
+		liked: 'No',
+		rating: '4',
+		author: 'Riichirō Inagaki',
+		editorHouse: "Weekly Shōnen Jump di Shūeisha",
+		description: "Bellissimo anime meravigliosi contenuti, paesaggi mozzafiato",
+		urlLink: "https://www.youtube.com/watch?v=-2Xt6UT0yPs",
+		year: '2018'
+	},
+	
+	];
